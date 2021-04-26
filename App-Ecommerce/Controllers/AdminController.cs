d82b7c6ebc2afc373a71036064b379bc89f2e8dc
Kozer2 <benhemann@yahoo.com>
2021-04-26 17:05:10 -0500

    got my admin dashboard working

---
+++ App-Ecommerce/Controllers/AdminController.cs
@@ -0,0 +1,99 @@
+﻿using App_Ecommerce.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_Ecommerce.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: AdminsController
+        public ActionResult Index()
+        {
+
+            var admin = new[]
+            {
+                new Admin
+                {
+                    Id = 1,
+                    FirstName = "Ben",
+                    LastName = "Hemann",
+                    UserName = "BHemann",
+                }
+            };
+            return View(admin);
+        }
+
+        // GET: AdminsController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AdminsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdminsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminsController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdminsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdminsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ App-Ecommerce/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using App_Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_Ecommerce.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index(string id)
+        {
+            if(id != null)
+                return NotFound();
+
+            _logger.LogWarning("Not Found!");
+            return View();
+        }
+
+        [HttpGet("Privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

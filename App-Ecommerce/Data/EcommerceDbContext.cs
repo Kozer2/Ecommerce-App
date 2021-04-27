d71ecdde2d2bfd9de5cc4e4cfad1438643a79287
Kozer2 <benhemann@yahoo.com>
2021-04-27 16:12:54 -0500

    added identity, and category and product controllers

---
+++ App-Ecommerce/Data/EcommerceDbContext.cs
@@ -0,0 +1,20 @@
+﻿using App_Ecommerce.Models.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_Ecommerce.Data
+{
+    public class EcommerceDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public EcommerceDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+    }
+}
--- App-Ecommerce/Startup.cs
+++ App-Ecommerce/Startup.cs
@@ -1,6 +1,8 @@
+using App_Ecommerce.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,14 @@ namespace App_Ecommerce
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<EcommerceDbContext>(options =>
+            {
+                var cs = Configuration.GetConnectionString("DefaultConnection");
+
+                if (cs == null) throw new InvalidOperationException("Default Connection is missing");
+                options.UseSqlServer(cs);
+            });
+
             services.AddControllersWithViews();
         }
 

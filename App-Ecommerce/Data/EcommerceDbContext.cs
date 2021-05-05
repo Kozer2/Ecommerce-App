edc7bdd97658a039e258fb0ae4d4c0aa3df6dfaf
Kozer2 <benhemann@yahoo.com>
2021-05-05 15:48:44 -0500

    moved around some nav links, created shopping cart model

--- App-Ecommerce/Data/EcommerceDbContext.cs
+++ App-Ecommerce/Data/EcommerceDbContext.cs
@@ -41,6 +41,7 @@ namespace App_Ecommerce.Data
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Cart> Carts { get; set; }
 
     }
 }
---
+++ App-Ecommerce/Models/Cart.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App_Ecommerce.Models
+{
+    public class Cart
+    {
+        [Key]
+        public string ItemId { get; set; }
+
+        public string CartId { get; set; }
+    }
+}
